9e4aec39ddc6c3e80a29670a9f5c8597e6ffc632
dmitry <frnutt64@gmail.com>
2025-09-18 16:36:53 +0300

    removed unnecessary use

--- run.php
+++ run.php
@@ -3,7 +3,6 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\StringReverser;
-use Tests\StringReverserTest;
 
 $stringReverser = new StringReverser();
 echo $stringReverser->reverseString('can`t');
